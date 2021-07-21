601679cfdd91d4719d88f911c23ea44cff797c4b
Xavier-Grajales <219303760@correo.ler.uam.mx>
2021-07-21 14:10:31 -0500

    CódigoDeServoMotor
    El código muestra la manera en la que se programa un servoMotor mediante la IDE de Arduino

---
+++ Código/ServoMotor.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>//header/cabecera y archivo Servo.ccp
+
+Servo miServo; //definimos al objeto
+int pinServo=4;
+
+void setup()
+{
+ Serial.begin(9600);
+ miServo.attach(pinServo);//Le decimos en que pin va conectado
+}
+
+void loop()
+{
+ miServo.write(0);//grado en el que se posiciona el servo
+ delay(2000);
+
+ miServo.write(90);//grado en el que se posiciona el servo
+ delay(2000);
+ 
+ //miServo.write(45);
+ //delay(2000);
+  
+ //miServo.write(180);
+ //delay(2000);
+  
+}
